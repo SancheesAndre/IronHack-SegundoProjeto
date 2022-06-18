9a5a421283dfd749062dee4bb173844dc15114f3
Emanuel Azevedo <emanuelcansancao@hotmail.com>
2022-06-17 22:27:52 -0300

    Navbar iniciado com lista para links e botão cadastrar

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,25 @@
+import './Navbar.css'
+function Navbar() {
+    return(
+        <nav>
+            <div className='logo'>
+                IronTour Advisor
+            </div>
+
+            <div className='menu'>
+                <ul>
+                    <li>
+                        <a>Home</a>
+                    </li>                
+                    <li>
+                        <a>Destinos</a>
+                    </li>
+                </ul>                
+                <button className='btn-cadastrar'>Cadastre seu destino</button>
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Navbar
