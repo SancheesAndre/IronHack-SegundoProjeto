6afa06663349a568e1e02353d2041f31ddfd4a74
Emanuel Azevedo <emanuelcansancao@hotmail.com>
2022-06-18 13:13:31 -0300

    Navbar com menu alinhado e Hero ainda em processo

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,9 +4,11 @@ import noronha from '../../images/noronha.jpeg'
 function Hero() {
     return(
         <div className='hero'>
+
             <div className="hero-text">
+                {/* {/* <img src={noronha} alt='Noronha'/>
                 <h1>A fim de viajar?</h1>
-                <p>NORONHA</p>                    
+                <p>NORONHA</p>  */}                   
             </div>
         </div>
     )
