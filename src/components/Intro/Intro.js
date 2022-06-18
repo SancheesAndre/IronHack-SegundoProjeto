20b54b9c1fd26b77d4b5a023a264511fc20388ab
Emanuel Azevedo <emanuelcansancao@hotmail.com>
2022-06-18 16:35:14 -0300

    Componente Intro criado

---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,15 @@
+import './Intro.css'
+
+function Intro() {
+    return(
+        <div className='Intro'>
+            <h2>
+                Veja mais Destinos
+            </h2>
+            <p>Descubra os melhores roteiros para praias, cachoeiras e lugares históricos!</p>
+            <button className='btn-destinos'>Destinos</button>
+        </div>
+    )
+}
+
+export default Intro
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar/Navbar"
 import Hero from "../components/Hero/Hero"
+import Intro from "../components/Intro/Intro"
 
 const HomePage = () => {
     return (
         <div>
             <Navbar />
             <Hero />
+            <Intro />
         </div>       
     )
 }
