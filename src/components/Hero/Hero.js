c594d360cf9bbd7a7145f384f3c25951357d0b15
Emanuel Azevedo <emanuelcansancao@hotmail.com>
2022-06-30 17:29:23 -0300

    Tentativa de carrossel abortada

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,53 +1,22 @@
+import "bootstrap/dist/css/bootstrap.min.css";
 import './Hero.css';
 import noronha from '../../images/noronha.jpeg';
 import cristo from '../../images/cristo.jpeg';
-import stadium from '../../images/stadium.jpeg';
-import "bootstrap/dist/css/bootstrap.min.css";
+import estadio from '../../images/stadium.jpeg';
 
 function Hero() {
-    return (
-        <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
-            <div className="carousel-inner">
-                <div className="carousel-item active">
-                    <img src={noronha} className="d-block w-100" alt="noronha"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={cristo} className="d-block w-100" alt="cristo"/>
-                </div>
-                <div className="carousel-item">
-                    <img src={stadium} className="d-block w-100" alt="stadium"/>
-                </div>
-            </div>
 
-            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Previous</span>
-            </button>
-            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                <span className="visually-hidden">Next</span>
-            </button>
+  return(
+    <div className='hero'>
+      <img className='heroImage' src={noronha} alt='Noronha' />
+
+      <div className="hero-text">
+        <h1>A fim de viajar?</h1>
+        <p className='placeName'>NORONHA</p>
       </div>
-
-
-
-
-
-
-
-
-
-        /* <div className='hero'>
-            <img className='heroImage' src={noronha} alt='Noronha' />
-
-            <div className="hero-text">
-
-                <h1>A fim de viajar?</h1>
-                <p className='placeName'>NORONHA</p>
-            </div>
-
-        </div> */
-    )
+    </div>
+  )
+  
 }
 
 export default Hero
